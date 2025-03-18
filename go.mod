86e1197ce6b0d345e904917b4a15ac21f397fdf4
ahgr3y <leehh97@gmail.com>
2025-03-18 10:56:59 +0800

    Add read from file function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AhGr3y/httpfromtcp
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const inputFilePath = "messages.txt"
+
+func main() {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("failed to open %s: %s\n", inputFilePath, err)
+	}
+
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer close(lines)
+		defer f.Close()
+		currentLine := ""
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLine != "" {
+					lines <- currentLine
+					currentLine = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error reading from %s: %s\n", inputFilePath, err)
+				break
+			}
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				currentLine += parts[i]
+				lines <- currentLine
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+	}()
+
+	return lines
+}
